bf38b09867ed304b638bf215ee59ad7918b637a0
RojiRoja <roja1896@gmail.com>
2021-07-27 21:36:46 +0530

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :books
 root to: 'book_store#books'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 #get '/hello', to: 'home#hello'
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_08_010851) do
+ActiveRecord::Schema.define(version: 2021_07_27_022115) do
 
   create_table "articles", force: :cascade do |t|
     t.string "name"
     t.string "address"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.integer "pages"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
